b47179237f15b29769bbc938d383f88634ee6641
kitepon-rgb <kitepon-rgb@users.noreply.github.com>
2026-07-22 12:15:18 +0900

    freshness cacheのsymbol seamを固定する

--- Sources/AIShellCore/CheckFreshnessCache.swift
+++ Sources/AIShellCore/CheckFreshnessCache.swift
@@ -1,3 +1,6 @@
 import Foundation
 
-// ACE-034aの専用ownership seam。実装はLattice compile後のwriterが所有する。
+/// ACE-034aの専用ownership seam。実装はLattice compile後のwriterが所有する。
+public actor CheckFreshnessCache {
+    public init() {}
+}
